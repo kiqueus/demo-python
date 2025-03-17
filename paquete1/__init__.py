9ed9e615a6a14fee39c9d5c3a91c39790f64f02f
kiqueus <kiqueus2002@gmail.com>
2025-03-17 15:03:05 -0300

    hola

---
+++ paquete1/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 14:47:39 2025
+
+@author: usuario
+"""
+
+version = 0.1
---
+++ paquete2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 14:48:16 2025
+
+@author: usuario
+"""
+
